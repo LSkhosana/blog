2809da013084e2ec52d051d769210843b62845fa
Lesedi <skhosanalesedi888@gmail.com>
2024-09-17 15:32:28 +0200

    update

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -15,7 +15,7 @@ const Posts = () => {
     useEffect(() => {
         const fetchAllBooks = async () => {
             try {
-                const res = await axios.get("http://localhost:8800/books");
+                const res = await axios.get("https://blog-ys2f.onrender.com/books");
                 setBooks(res.data);
             } catch (err) {
                 console.error(err);
@@ -27,7 +27,7 @@ const Posts = () => {
 
     const handleDelete = async (id)=>{
         try{
-            await axios.delete("http://localhost:8800/books/"+id);
+            await axios.delete("https://blog-ys2f.onrender.com/books/"+id);
             window.location.reload()
         }catch(err){
             console.error(err)
